b9f2057ea08f3b76a531be1600487a09b2180786
GuerrillaTechnology <59454063+GuerrillaTechnology@users.noreply.github.com>
2020-11-07 21:07:28 +0100

    Fixed Docker Hub repository name

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ docker pull --platform linux/arm/v7 debian:buster-slim
 docker pull --platform linux/arm64 debian:buster-slim
 
 docker login
-docker buildx build --platform linux/arm/v7,linux/arm64 -t tobiasbartel/test:${TAG} -t tobiasbartel/test:latest --push .
+docker buildx build --platform linux/arm/v7,linux/arm64 -t guerrillatechnology/streamserver:${TAG} -t guerrillatechnology/streamserver:latest --push .
 
